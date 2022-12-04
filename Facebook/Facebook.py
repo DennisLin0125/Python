99a0f4738205e6dc118a5968e46cbeb2f9827e46
Dennis <740125aa@gmail.com>
2022-12-04 15:34:24 +0800

    modify

--- Facebook/Facebook.py
+++ Facebook/Facebook.py
@@ -3,7 +3,7 @@ import jieba
 import operator
 
 url = "https://graph.facebook.com/v15.0/me/posts?access_token="
-token = "EAAP1quLZAFo4BALDnA4WvWlmUdy1syRZBfE3c5t1oZBTZCqARtv1hE5z3H1ZBzPL5F5qfbWKzyTNl3yuqdvZAjF29WM0qssbAayS2LsgEtgb5x4HztofOzZCl6ZALbA0rYfMDStV2RadjBQ0pwhq5XndOBCBOMueG9MgpiMhdpv8FYMTmKP0mNanFizbzwY84ThVtgwVRoibxTeBMZBweGINRzOc1bj88NurrgBKrGflSHUZAzUaecjLu6"
+token = "EAAP1quLZAFo4BAOcJYLzU5nzNb721j29jzOkrVPsO440GS3ww4Md7llTtyLYSppYUS4oW1mVYeVMHfFQSJVjsu15oAzfm07P1THHvwrEvHZB7lwE7ejMqZAxXbMEwzNgziqgz8IXEF2SQwNZAYOw8siJExVSHjXxyZC7tzSqLMIswXmC21B2AzAs9qhw8jyMzX5SAsSXjbmO5WTqVhIIQ09vanibrtkFqZBKHlffHDoqqp7ZCoV915r"
 
 res = requests.get(url=url+token)
 jd = res.json()
@@ -13,17 +13,17 @@ dic={}
 if __name__ == "__main__":
     if res.status_code == requests.codes.ok:
         while 'paging' in jd:
-            for i in jd['data']:
-                if 'message' in i:
-                    corpus += jieba.cut(i['message'])
+            for post in jd['data']:
+                if 'message' in post:
+                    corpus += jieba.cut(post['message'])
             res = requests.get(jd['paging']['next'])
             jd = res.json()
 
-        for i in corpus:
-            if i not in dic:
-                dic[i] = 1
+        for word in corpus:
+            if word not in dic:
+                dic[word] = 1
             else:
-                dic[i] += 1
+                dic[word] += 1
 
         sorted_word = sorted(dic.items(), key=operator.itemgetter(1), reverse=True)
 
