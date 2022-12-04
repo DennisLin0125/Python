6a40fadb434274eb265d5d7ad3beb11ecb7c7f7d
Dennis <740125aa@gmail.com>
2022-12-04 15:45:43 +0800

    modify

--- Facebook/Facebook.py
+++ Facebook/Facebook.py
@@ -1,21 +1,26 @@
 import requests
 import jieba
 import operator
+from openpyxl import Workbook
+
+wb=Workbook()
+ws=wb.create_sheet('臉書關鍵字',0)
 
 url = "https://graph.facebook.com/v15.0/me/posts?access_token="
-token = "EAAP1quLZAFo4BAJanZCMeTxXnVdpcZBZBRd412LS6uZCeEOHkNegvNxUsqLSQErNG23NA6twS8UYbwtlpxJhRLoGeBcfQSaLf4dtiG8m782Nrj9FKmzXxxmfJZBFZCFl3950PW4VCpb4T4UZBsdatYQNLVvBidtuvdsgBpp3Et7238a5rYfC4ds7CIXJdqzFZAvZAqEqsw084mL9OJZAVlizUMbEoM89GqZBkcauGCDYPpcofIRn5Iz0nTdQ"
+token = "EAAP1quLZAFo4BACbXeQKAeyOjSKrS9dIj7AreH3m6iwx9ZAPZB4d9d4pVnAq6AmjG2mwT7nYYzF9BSTciQb4YomiPtHgxVnIYO6VTzLm2duaNzjoQpmjoNjsDtvoFsn4Lp1q1MGrx4FmMURKG6zZB6TZB5yFnfzTwJZCcarcPYWHB0If0HZADJh3viWAxx8CTz1JeH0g0b5PhtBVj5p9K7wsuSzZBpHbyqLZCLZC1tHhnNtO31VgFXkZC5X"
 
 res = requests.get(url=url+token) #得到Facebook資料
 jd = res.json()   #將資料轉成json
 
 corpus=[]
+arr=[]
 dic={}
 if __name__ == "__main__":
     if res.status_code == requests.codes.ok:  #如果成功的話
         while 'paging' in jd:       #當還有下一頁的時候
             for post in jd['data']:
                 if 'message' in post:  #如果有發文資料
-                    corpus += jieba.cut(post['message'])  #將發文資料切割
+                    corpus += jieba.cut(post['message'])  #將發文資料切割並加到corpus的list裡面
             res = requests.get(jd['paging']['next'])   #繼續下一頁
             jd = res.json() #將資料轉成json
 
@@ -29,6 +34,11 @@ if __name__ == "__main__":
 
         for ele in sorted_word:
             if len(ele[0]) >= 2:   #只印出Key長度>2的資料
+                arr += [ele[0]]
+                arr += [ele[1]]
                 print(ele[0], ele[1])
+                ws.append(arr)
+                arr=[]
+        wb.save('臉書關鍵字.xlsx')
     else:
         print('token 已過期')
