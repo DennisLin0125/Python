c8421d6e9f4dc182c12717b5fe92be3c0d4da5cc
Dennis <740125aa@gmail.com>
2022-12-12 21:15:59 +0800

    modify

--- 1111人力/1111.py
+++ 1111人力/1111.py
@@ -1,7 +1,6 @@
 from requests import get
 from bs4 import BeautifulSoup
 from openpyxl import Workbook
-from time import sleep
 
 wbTitle = ['連結', '職缺', '公司名稱', '地點', '薪水']
 
@@ -22,14 +21,14 @@ job_data = soup.find_all('div', class_="job_item_info")
 
 sumL = 0
 
-while job_data!=[]:
-    print('正在爬取第',page,'頁')
+while job_data:
+    print('正在爬取第', page, '頁')
     a = 0
     for job in job_data:
         職缺 = job.h5.text
         公司名稱 = job.h6.text
         連結 = job.a['href']
-        地點 = job.find_all('a',class_="job_item_detail_location mr-3 position-relative")[0].text
+        地點 = job.find_all('a', class_="job_item_detail_location mr-3 position-relative")[0].text
         薪水 = job.find_all('div', class_="job_item_detail_salary ml-3 font-weight-style digit_6")[0].text
         ws.append([連結, 職缺, 公司名稱, 地點, 薪水])
         a += 1
